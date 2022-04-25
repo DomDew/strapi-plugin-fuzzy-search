7502c7da64f91244d7b9575d04105f234c0c1f11
Dominik Dewitz <dewitzdominik@kabelmail.de>
2022-04-25 10:44:10 +0200

    chore: remove unused functions

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -7,23 +7,14 @@ const { getPluginService } = require("./utils/getPluginService");
 module.exports = ({ strapi }) => {
   const settingsService = getPluginService(strapi, "settingsService");
   const settings = settingsService.get();
-
   // build settings structure
   const normalizedSettings = settingsService.build(settings);
-
   // reset plugin settings
   settingsService.set(normalizedSettings);
-
   // set up lifecycles
   const subscribe = {
     models: _.map(normalizedSettings.models, (m) => m.uid),
   };
-
-  SUPPORTED_LIFECYCLES.forEach((lifecycle) => {
-    subscribe[lifecycle] = (ctx) => {
-      getPluginService(strapi, "slugService").slugify(ctx);
-    };
-  });
 
   strapi.db.lifecycles.subscribe(subscribe);
 };
--- server/index.js
+++ server/index.js
@@ -2,14 +2,12 @@
 
 const bootstrap = require("./bootstrap");
 const register = require("./register");
-const destroy = require("./destroy");
 const config = require("./config");
 const services = require("./services");
 
 module.exports = {
   bootstrap,
   register,
-  destroy,
   config,
   services,
 };
