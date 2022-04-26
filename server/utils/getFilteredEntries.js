e0ac25b33bb3b33ec03ea17cb1944c6636e990a9
Katrin Wüstefeld <wuestefeld@machbar.eu>
2022-04-26 14:11:13 +0200

    feat(dbQuery): added Filter-Options

--- server/utils/getFilteredEntries.js
+++ server/utils/getFilteredEntries.js
@@ -1,11 +1,15 @@
-const buildDbQuery = (locale) => {
-  if (locale) return { where: { locale: locale } };
+const { getPluginService } = require("../utils/getPluginService");
+
+const buildDbQuery = (locale, model) => {
+  const { contentTypes } = getPluginService(strapi, "settingsService").get();
+  const { whereConstraints } = contentTypes.find(entry => entry.uid === model);
+  if (locale) return { where: { locale: locale,...whereConstraints } };
 
   return {};
 };
 
 const getFilteredEntries = async (locale, model) => {
-  return await strapi.db.query(model).findMany(buildDbQuery(locale));
+  return await strapi.db.query(model).findMany(buildDbQuery(locale,model));
 };
 
 module.exports = {
