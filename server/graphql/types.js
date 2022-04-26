11de6b2c4316cd13a41f1693a5644cdc466eb856
Dominik Dewitz <dewitzdominik@kabelmail.de>
2022-04-26 12:11:03 +0200

    feat: refactor search logic into service

--- server/graphql/types.js
+++ server/graphql/types.js
@@ -1,19 +1,13 @@
 const fuzzysort = require("fuzzysort");
 const _ = require("lodash");
+const { fuzzySearchService } = require("../services");
 const { getLocalizedEntries } = require("../utils/getLocalizedEntries");
 const { getPluginService } = require("../utils/getPluginService");
 
 const getCustomTypes = (strapi, nexus) => {
   const { naming } = getPluginService(strapi, "utils", "graphql");
-  const { toEntityResponseCollection } = getPluginService(
-    strapi,
-    "format",
-    "graphql"
-  ).returnTypes;
   const { contentTypes } = getPluginService(strapi, "settingsService").get();
   const { getEntityResponseCollectionName } = naming;
-
-
 
   // Get response names
   contentTypes.forEach((type) => {
@@ -52,66 +46,12 @@ const getCustomTypes = (strapi, nexus) => {
             // Destructure the args to get query value
             const { query, locale } = args;
 
-            // Get all projects, news and articles for a given locale, to be able to filter through them
-            // Doing this in the resolver so we always have the newest entries
-            const localizedEntries = await Promise.all(
-              contentTypes.map(async (contentType) => {
-                return {
-                  pluralName: contentType.model.info.pluralName,
-                  fuzzysortOptions: contentType.fuzzysortOptions,
-                  [contentType.model.info.pluralName]:
-                    await getLocalizedEntries(locale, contentType.model.uid),
-                };
-              })
-            );
+            const results = await getPluginService(
+              strapi,
+              "fuzzySearchService"
+            ).getResults(query, locale);
 
-            const searchResults = localizedEntries.map((model) => {
-              const keys = model.fuzzysortOptions.keys.map((key) => key.name);
-
-              // Splice strings to search if characterLimit has been passed
-              if (model.fuzzysortOptions.characterLimit) {
-                model[model.pluralName].forEach((entry) => {
-                  const entryKeys = Object.keys(entry);
-
-                  entryKeys.forEach((key) => {
-                    if (!keys.includes(key)) return;
-
-                    if (!entry[key]) return;
-
-                    entry[key] = entry[key].slice(
-                      0,
-                      model.fuzzysortOptions.characterLimit
-                    );
-                  });
-                });
-              }
-
-              return {
-                pluralName: model.pluralName,
-                fuzzysort: fuzzysort.go(query, model[model.pluralName], {
-                  threshold: parseInt(model.fuzzysortOptions.threshold),
-                  limit: parseInt(model.fuzzysortOptions.limit),
-                  allowTypo: model.fuzzysortOptions.allowTypo,
-                  keys: model.fuzzysortOptions.keys.map((key) => key.name),
-                  scoreFn: (a) =>
-                    Math.max(
-                      ...model.fuzzysortOptions.keys.map((key) =>
-                        a[0] ? a[0].score + key.weight : -1000
-                      )
-                    ),
-                }),
-              };
-            });
-
-            // Destructure search results and return them in appropriate format
-            const results = {};
-
-            searchResults.forEach((res) => {
-              results[res.pluralName] = toEntityResponseCollection(
-                res.fuzzysort.map((fuzzyRes) => fuzzyRes.obj)
-              );
-            });
-
+            console.log(results);
             /**
              * Error handling
              * TODO: Implement error handling
---
+++ server/services/fuzzySearchService.js
@@ -0,0 +1,77 @@
+const fuzzysort = require("fuzzysort");
+const _ = require("lodash");
+const { getLocalizedEntries } = require("../utils/getLocalizedEntries");
+const { getPluginService } = require("../utils/getPluginService");
+
+module.exports = ({ strapi }) => ({
+  async getResults(query, locale) {
+    const { toEntityResponseCollection } = getPluginService(
+      strapi,
+      "format",
+      "graphql"
+    ).returnTypes;
+    const { contentTypes } = getPluginService(strapi, "settingsService").get();
+
+    // Get all projects, news and articles for a given locale, to be able to filter through them
+    // Doing this in the resolver so we always have the newest entries
+    const localizedEntries = await Promise.all(
+      contentTypes.map(async (contentType) => {
+        return {
+          pluralName: contentType.model.info.pluralName,
+          fuzzysortOptions: contentType.fuzzysortOptions,
+          [contentType.model.info.pluralName]: await getLocalizedEntries(
+            locale,
+            contentType.model.uid
+          ),
+        };
+      })
+    );
+
+    const searchResults = localizedEntries.map((model) => {
+      const keys = model.fuzzysortOptions.keys.map((key) => key.name);
+
+      // Splice strings to search if characterLimit has been passed
+      if (model.fuzzysortOptions.characterLimit) {
+        model[model.pluralName].forEach((entry) => {
+          const entryKeys = Object.keys(entry);
+
+          entryKeys.forEach((key) => {
+            if (!keys.includes(key)) return;
+
+            if (!entry[key]) return;
+
+            entry[key] = entry[key].slice(
+              0,
+              model.fuzzysortOptions.characterLimit
+            );
+          });
+        });
+      }
+
+      return {
+        pluralName: model.pluralName,
+        fuzzysort: fuzzysort.go(query, model[model.pluralName], {
+          threshold: parseInt(model.fuzzysortOptions.threshold),
+          limit: parseInt(model.fuzzysortOptions.limit),
+          allowTypo: model.fuzzysortOptions.allowTypo,
+          keys: model.fuzzysortOptions.keys.map((key) => key.name),
+          scoreFn: (a) =>
+            Math.max(
+              ...model.fuzzysortOptions.keys.map((key) =>
+                a[0] ? a[0].score + key.weight : -1000
+              )
+            ),
+        }),
+      };
+    });
+
+    // Destructure search results and return them in appropriate format
+    const results = {};
+
+    searchResults.forEach((res) => {
+      results[res.pluralName] = toEntityResponseCollection(
+        res.fuzzysort.map((fuzzyRes) => fuzzyRes.obj)
+      );
+    });
+  },
+});
