c23a09e8bae0b4bc17c7a6dfe766fb8e71cedde9
Dominik Dewitz <dd@thepeaklab.com>
2022-05-10 11:36:48 +0200

    fix(validate-query): fix wrong check for enabled localization

--- server/utils/validateQuery.js
+++ server/utils/validateQuery.js
@@ -1,19 +1,26 @@
 const { ValidationError } = require("@strapi/utils/lib/errors");
 
 const validateQuery = async (contentType, locale) => {
-    const isLocalizedContentType = await strapi.plugins.i18n.services["content-types"].isLocalizedContentType(contentType);
+  const isLocalizedContentType = await strapi.plugins.i18n.services[
+    "content-types"
+  ].isLocalizedContentType(contentType.model);
 
-    if (locale && !isLocalizedContentType) {
-        throw new ValidationError(`A query for the locale: '${locale}' was found, however model: '${contentType.model.modelName}' is not a localized content type. Enable localization if you want to query or localized entries.`)
-    }
+  if (locale && !isLocalizedContentType) {
+    throw new ValidationError(
+      `A query for the locale: '${locale}' was found, however model: '${contentType.model.modelName}' is not a localized content type. Enable localization if you want to query or localized entries.`
+    );
+  }
 
-    contentType.fuzzysortOptions.keys.forEach(key => {
-        const attributeKeys = Object.keys(contentType.model.attributes)
-        
-        if (!attributeKeys.includes(key.name)) throw new ValidationError(`Key: '${key.name}' is not a valid field for model: '${contentType.model.modelName}`)
-    })
-}
+  contentType.fuzzysortOptions.keys.forEach((key) => {
+    const attributeKeys = Object.keys(contentType.model.attributes);
+
+    if (!attributeKeys.includes(key.name))
+      throw new ValidationError(
+        `Key: '${key.name}' is not a valid field for model: '${contentType.model.modelName}`
+      );
+  });
+};
 
 module.exports = {
-    validateQuery,
-}
+  validateQuery,
+};
