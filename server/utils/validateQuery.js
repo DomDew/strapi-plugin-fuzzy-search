353177992d13f37d95efddb643de75341d7594e8
Dominik Dewitz <dd@thepeaklab.com>
2022-06-02 09:03:10 +0200

    fix(validation): validate content types before locales

--- server/utils/validateQuery.js
+++ server/utils/validateQuery.js
@@ -1,10 +1,17 @@
 const { ValidationError } = require("@strapi/utils/lib/errors");
 
 const validateQuery = async (contentType, locale) => {
-  if (!locale) {
-    return;
-  }
-  
+  contentType.fuzzysortOptions.keys.forEach((key) => {
+    const attributeKeys = Object.keys(contentType.model.attributes);
+
+    if (!attributeKeys.includes(key.name))
+      throw new ValidationError(
+        `Key: '${key.name}' is not a valid field for model: '${contentType.model.modelName}`
+      );
+  });
+
+  if (!locale) return;
+
   const isLocalizedContentType = await strapi.plugins.i18n.services[
     "content-types"
   ].isLocalizedContentType(contentType.model);
@@ -14,15 +21,6 @@ const validateQuery = async (contentType, locale) => {
       `A query for the locale: '${locale}' was found, however model: '${contentType.model.modelName}' is not a localized content type. Enable localization if you want to query or localized entries.`
     );
   }
-
-  contentType.fuzzysortOptions.keys.forEach((key) => {
-    const attributeKeys = Object.keys(contentType.model.attributes);
-
-    if (!attributeKeys.includes(key.name))
-      throw new ValidationError(
-        `Key: '${key.name}' is not a valid field for model: '${contentType.model.modelName}`
-      );
-  });
 };
 
 module.exports = {
